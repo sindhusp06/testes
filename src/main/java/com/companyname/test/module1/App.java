fbf323807b47e5ba76f9fbc69bf6019e8a6a2928
sindhusp06 <sindhusuresh0801@gmail.com>
2018-01-27 23:57:48 +0530

    Initial commit

---
+++ src/main/java/com/companyname/test/module1/App.java
@@ -0,0 +1,13 @@
+package com.companyname.test.module1;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
